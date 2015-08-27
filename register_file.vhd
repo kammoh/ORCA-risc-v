cff553b22de984a52d64ca0ec49b0686e6c61ac2
Joel Vandergriendt <vandergriendtjoel@gmail.com>
2015-08-27 12:33:25 -0700

    get the register file to use a block ram

--- register_file.vhd
+++ register_file.vhd
@@ -29,27 +29,44 @@ architecture rtl of register_file is
   signal registers : register_list := (others => (others => '0'));
 
   constant ZERO : std_logic_vector(REGISTER_NAME_SIZE-1 downto 0) := (others => '0');
+
+  signal we           : std_logic;
+  signal out1         : std_logic_vector(REGISTER_SIZE-1 downto 0);
+  signal out2         : std_logic_vector(REGISTER_SIZE-1 downto 0);
+  signal fwd1         : std_logic;
+  signal fwd2         : std_logic;
+  signal data_latched : std_logic_vector(REGISTER_SIZE-1 downto 0);
 begin
+
+  we <= '1' when writeback_enable = '1' and writeback_sel /= ZERO and stall = '0' else '0';
+
   register_proc : process (clk) is
   begin
     if rising_edge(clk) then
-      if stall = '0' and valid_input = '1' then
-                                        --read before bypass
-        rs1_data <= registers(to_integer(unsigned(rs1_sel)));
-        rs2_data <= registers(to_integer(unsigned(rs2_sel)));
-        if writeback_enable = '1' and writeback_sel /= ZERO then
-
-          registers(to_integer(unsigned(writeback_sel))) <= writeback_data;
-          if rs1_sel = writeback_sel then
-            rs1_data <= writeback_data;
-          end if;
-          if rs2_sel = writeback_sel then
-            rs2_data <= writeback_data;
-          end if;
-        end if;
-
-      end if;  --stall
-
+      if we = '1' then
+        registers(to_integer(unsigned(writeback_sel))) <= writeback_data;
+      end if;
+      out1 <= registers(to_integer(unsigned(rs1_sel)));
+      out2 <= registers(to_integer(unsigned(rs2_sel)));
+      if we = '1' and writeback_sel = rs1_sel then
+        fwd1 <= '1';
+      else
+        fwd1 <= '0';
+      end if;
+      if we = '1' and writeback_sel = rs2_sel then
+        fwd2 <= '1';
+      else
+        fwd2 <= '0';
+      end if;
     end if;  --rising edge
   end process;
+  process(clk) is
+  begin
+    if rising_edge(clk) then
+      data_latched <= writeback_data;
+    end if;
+  end process;
+
+  rs1_data <= data_latched when fwd1 = '1' else out1;
+  rs2_data <= data_latched when fwd2 = '1' else out2;
 end architecture;
