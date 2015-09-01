0aca3f0c1b3b7b9ae266ada70c6b0ab524cfa0ef
Joel Vandergriendt <vandergriendtjoel@gmail.com>
2015-09-01 08:37:37 -0700

    update tools

--- tools/generate_hex_files.sh
+++ tools/generate_hex_files.sh
@@ -1,10 +1,14 @@
 TEST_DIR=/nfs/scratch/riscv-tools/riscv-tests/isa
 FILES=$(ls $TEST_DIR/rv32ui-p-* | grep -v hex | grep -v dump)
+
+mkdir -p test
 for i in $FILES
 do
 	 echo "$i > test/$(basename $i).gex"
-    riscv64-unknown-elf-objcopy  -O binary $i temp.bin
-    python ../tools/bin2mif.py temp.bin 0x100 > temp.mif
-    mif2hex temp.mif test/$(basename $i).gex >/dev/null 2>&1
+	 riscv64-unknown-elf-objcopy  -O binary $i temp.bin &
+	 riscv64-unknown-elf-objdump --disassemble-all -Mnumeric,no-aliases $i > test/$(basename $i).dump &
+	 wait
+    python ../tools/bin2mif.py temp.bin 0x100 > temp.mif || exit -1
+    mif2hex temp.mif test/$(basename $i).gex >/dev/null 2>&1 || exit -1
 done
 rm temp.bin temp.mif
--- tools/run_all.sh
+++ tools/run_all.sh
@@ -1,13 +1,12 @@
 STR=""
+DATE=$(date)
 for i in test/*.gex
 do
 	 cp $i test.hex
 	 quartus_cdb --update_mif vblox1.qpf
 	 quartus_asm vblox1.qpf
 	 quartus_pgm -m JTAG -o P\;output_files/vblox1.sof
-	 echo "$i - homtohost?"
-	 read -n1 k
-
-	 echo "$i = $k" >> test_results.txt
+	 k=$(system-console --script=system_console.tcl | grep to_host | sed 's/to_host=\(.*\)/\1/')
+	 echo "$i = $k" | tee -a "test_results$DATE.txt"
 done
 echo $STR
