d86887c2b9a4ab9a39642437a309cb240e354953
Joel Vandergriendt <vandergriendtjoel@gmail.com>
2015-09-02 15:34:04 -0700

    Simulator works with single ported memory

--- instruction_fetch.vhd
+++ instruction_fetch.vhd
@@ -9,7 +9,7 @@ entity instruction_fetch is
   generic (
     REGISTER_SIZE    : positive;
     INSTRUCTION_SIZE : positive;
-    RESET_VECTOR : natural );
+    RESET_VECTOR     : natural);
   port (
     clk   : in std_logic;
     reset : in std_logic;
@@ -34,85 +34,87 @@ end entity instruction_fetch;
 
 architecture rtl of instruction_fetch is
 
-  signal program_counter : std_logic_vector(REGISTER_SIZE -1 downto 0);
+  signal correction      : std_logic_vector(REGISTER_SIZE-1 downto 0);
+  signal correction_en   : std_logic;
+  signal program_counter : std_logic_vector(REGISTER_SIZE-1 downto 0);
+  signal generated_pc    : std_logic_vector(REGISTER_SIZE -1 downto 0);
+  signal address         : std_logic_vector(REGISTER_SIZE -1 downto 0);
 
-  signal instr       : std_logic_vector(INSTRUCTION_SIZE-1 downto 0);
-  signal instr_be    : std_logic_vector(INSTRUCTION_SIZE-1 downto 0);
+  signal instr : std_logic_vector(INSTRUCTION_SIZE-1 downto 0);
+
   signal valid_instr : std_logic;
 
-  signal generated_pc       : std_logic_vector(REGISTER_SIZE-1 downto 0);
-  signal latched_pc         : std_logic_vector(REGISTER_SIZE-1 downto 0);
-  signal latched_correction : std_logic_vector(REGISTER_SIZE-1 downto 0);
-  signal latched_corr_en    : std_logic;
+  signal saved_instr_out       : std_logic_vector(INSTRUCTION_SIZE-1 downto 0);
+  signal saved_pc_out          : std_logic_vector(REGISTER_SIZE-1 downto 0);
+  signal saved_next_pc_out     : std_logic_vector(REGISTER_SIZE-1 downto 0);
+  signal saved_valid_instr_out : std_logic;
 
-  signal stalled : std_logic;
 
 begin  -- architecture rtl
 
-  stalled <= stall or read_wait;
 
-  --if we are stalled, don't change the program counter, else
-  --if not stalled, and there is a prediction fail, load in the correction, else
-  --if while we were stalled a correction happened, load in the saved correction
-  --else use the normally generated program counter.
-
-  program_counter <= latched_pc when stalled = '1' else             --stalled
-                     pc_corr            when pc_corr_en = '1' else  --branch prediction fail
-                     latched_correction when latched_corr_en = '1' else
-                     generated_pc;      --regular operation
 
   assert program_counter(1 downto 0) = "00" report "BAD INSTRUCTION ADDRESS" severity error;
 
-  read_address <= program_counter;
-  read_en      <= not reset;
+  read_en <= not reset;
 
   latch_pc : process(clk)
   begin
     if rising_edge(clk) then
       if reset = '1' then
-        latched_pc      <= std_logic_vector(to_signed(RESET_VECTOR, REGISTER_SIZE));
-        latched_corr_en <= '0';
+        program_counter <= std_logic_vector(to_signed(RESET_VECTOR, REGISTER_SIZE));
+        correction_en   <= '0';
       else
-        latched_pc <= program_counter;
-
-        --latch in pc correction to be used after current read is complete
-        latched_correction <= pc_corr;
-        if pc_corr_en = '1' and stall = '1' then
-          latched_corr_en <= '1';
+        if pc_corr_en = '1' then
+          correction_en <= '1';
+          correction    <= pc_corr;
         elsif read_datavalid = '1' then
-          latched_corr_en <= '0';
+          correction_en <= '0';
         end if;
-      end if;  --reset
+        program_counter <= address;
+      end if;
     end if;  -- clock
   end process;
 
+  address <= program_counter when read_datavalid = '0' or stall = '1' else
+             correction when correction_en = '1' else
+             generated_pc;
 
 
-
-  instr_be <= read_data;
 --unpack instruction
-  instr <= (instr_be(7 downto 0) & instr_be(15 downto 8) &
-            instr_be(23 downto 16) & instr_be(31 downto 24));
+  instr <= (read_data(7 downto 0) & read_data(15 downto 8) &
+            read_data(23 downto 16) & read_data(31 downto 24));
 
-  valid_instr <= read_datavalid and not latched_corr_en;
+  valid_instr <= read_datavalid and not correction_en and not stall;
 
   pc_logic : component pc_incr
     generic map (
       REGISTER_SIZE    => REGISTER_SIZE,
       INSTRUCTION_SIZE => INSTRUCTION_SIZE)
     port map (
-      pc          => latched_pc,
+      pc          => program_counter,
       instr       => instr,
       valid_instr => valid_instr,
       next_pc     => generated_pc);
 
 
-  instr_out   <= instr;
-  pc_out      <= latched_pc;
-  next_pc_out <= generated_pc;
+  instr_out   <= saved_instr_out   when stall = '1' else instr;
+  pc_out      <= saved_pc_out      when stall = '1' else program_counter;
+  next_pc_out <= saved_next_pc_out when stall = '1' else generated_pc;
 
-  valid_instr_out <= valid_instr;
+  valid_instr_out <= saved_valid_instr_out when stall = '1' else valid_instr;
 
+  read_address <= address;
 
-
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if stall = '0' then
+        saved_instr_out       <= instr;
+        saved_pc_out          <= program_counter;
+        saved_next_pc_out     <= generated_pc;
+        saved_valid_instr_out <= valid_instr;
+      end if;
+    end if;
+  end process;
 end architecture rtl;
--- tools/testall_sim.tcl
+++ tools/testall_sim.tcl
@@ -8,8 +8,13 @@ set files [lsort [glob ../../../test/*.gex]]
 foreach f $files {
 	 file copy -force $f ../../../test.hex
 	 restart -f
-	 run 5000ns
-	 set v [examine -time 5000ns -decimal sim:/vblox1/riscv_0/coe_to_host]
+	 run 100 ns
+	 while { [examine -decimal sim:/vblox1/riscv_0/coe_to_host] == 0 } {
+		  if { $now > 5000000 } { break }
+		  run 100 ns;
+	 }
+
+	 set v [examine -decimal sim:/vblox1/riscv_0/coe_to_host]
 	 puts "$f = $v"
 }
 
